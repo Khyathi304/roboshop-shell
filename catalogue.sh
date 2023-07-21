5ad9a4b5af48c9ec3fb9f441c10248b5414690a7
Khyathi304 <khyathikamireddy04@gmail.com>
2023-07-21 17:25:32 +0100

    Printing Colors

--- catalogue.sh
+++ catalogue.sh
@@ -1,39 +1,39 @@
-echo ">>>>>>>>> Create Catalouge Service >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Create Catalouge Service <<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
 
-echo ">>>>>>>>> Create MongoRepo >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Create MongoRepo <<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo ">>>>>>>>> Install NodeJs Repo >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Install NodeJs Repo <<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo ">>>>>>>>> Install NodeJS >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<<<\e[0m"
 yum install nodejs -y
 
-echo ">>>>>>>>> Create Application User >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Create Application User <<<<<<<<<<\e[0m"
 useradd roboshop
 
-echo ">>>>>>>>> Create Application Directory >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<<<\e[0m"
 mkdir /app
 
-echo ">>>>>>>>> Download application content >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Download application content <<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
-echo ">>>>>>>>> Extract application content >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Extract application content <<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
 
-echo ">>>>>>>>> Download NodeJs Dependencies >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Download NodeJs Dependencies <<<<<<<<<<\e[0m"
 npm install
 
-echo ">>>>>>>>> Install Mongo Client >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Install Mongo Client <<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 
-echo ">>>>>>>>> Load Catalogue schema >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Load Catalogue schema <<<<<<<<<<\e[0m"
 mongo --host mongodb.kdevops304.online </app/schema/catalogue.js
 
-echo ">>>>>>>>> Restart the server >>>>>>>>>"
+echo -e "\e[36m>>>>>>>>> Restart the server <<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
