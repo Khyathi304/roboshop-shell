18a8265117355a565657e01e293435e0d451a086
Khyathi304 <khyathikamireddy04@gmail.com>
2023-07-24 15:50:24 +0100

    Exit Status in Roboshop.

--- common.sh
+++ common.sh
@@ -76,7 +76,7 @@ func_nodejs(){
 
   echo -e "\e[36m>>>>>>>>> Create MongoRepo <<<<<<<<<<\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-  if [ $? -eq 0]; then
+  if [ $? -eq 0 ]; then
     echo -e "\e[32m SUCCESS \e[0m"
     else
     echo -e "\e[31m FAILURE \e[0m"
