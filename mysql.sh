0c9f5d57aa3c6ea15aa697760b283ac3b5b2182c
Khyathi304 <khyathikamireddy04@gmail.com>
2023-07-25 16:51:25 +0100

    Password NFR

--- mysql.sh
+++ mysql.sh
@@ -3,8 +3,8 @@ if [ -z "${mysql_root_password}" ]; then
   echo Input Password Missing
   exit
 fi
-cp mysql.repo /etc/yum.repos.d/mysql.repo
 yum module disable mysql -y
+cp mysql.repo /etc/yum.repos.d/mysql.repo
 yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl restart mysqld
